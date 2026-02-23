72e3638b2149173142746ffdb148b25dec7cce62
NAKAIKosuke <260029462+pettimong@users.noreply.github.com>
2026-02-23 16:06:56 +0900

    Update kernel.c

--- kernel.c
+++ kernel.c
@@ -18,23 +18,47 @@ int cursor_row = 0;
 int cursor_col = 0;
 uint8_t current_color = 0x0F; // 白文字・黒背景
 
+/* scroll関数を作る */
+void scroll() {
+    // 2行目以降を1行上へコピー
+    for (int row = 1; row < VGA_HEIGHT; row++) {
+        for (int col = 0; col < VGA_WIDTH; col++) {
+            vga[(row - 1) * VGA_WIDTH + col] =
+                vga[row * VGA_WIDTH + col];
+        }
+    }
+
+    // 最終行を空白にする
+    for (int col = 0; col < VGA_WIDTH; col++) {
+        vga[(VGA_HEIGHT - 1) * VGA_WIDTH + col] =
+            (current_color << 8) | ' ';
+    }
+	
+	cursor_row = VGA_HEIGHT - 1;
+    cursor_col = 0;   // ← これが重要
+}
+
 /* 1文字出力関数を作る */
 void putchar(char c) {
     if (c == '\n') {
         cursor_row++;
         cursor_col = 0;
-        return;
-    }
+    } else {
 
-    int index = cursor_row * VGA_WIDTH + cursor_col;
-    vga[index] = (current_color << 8) | c;
+		int index = cursor_row * VGA_WIDTH + cursor_col;
+		vga[index] = (current_color << 8) | c;
 
-    cursor_col++;
+		cursor_col++;
 
-    if (cursor_col >= VGA_WIDTH) {
-        cursor_col = 0;
-        cursor_row++;
-    }
+		if (cursor_col >= VGA_WIDTH) {
+			cursor_col = 0;
+			cursor_row++;
+		}	
+	}
+
+	if (cursor_row >= VGA_HEIGHT) {
+		scroll();
+	}
 }
 
 /* 文字列出力関数 */
@@ -51,10 +75,12 @@ void kernel_main(void) {
         vga[i] = (current_color << 8) | ' ';
     }
 
-    print("Hello, My OS\n");
-    print("Second Line\n");
-    print("Third Line");
-
+	for (int i = 0; i < 30; i++) {
+		print("Line ");
+		putchar('0' + (i / 10));
+		putchar('0' + (i % 10));
+		print("\n");
+	}
     while (1) {
         __asm__("hlt");
     }
