e3ab83d6b1e2b17ea384b2449183535f72c3fec7
NAKAIKosuke <260029462+pettimong@users.noreply.github.com>
2026-02-23 15:17:16 +0900

    Update kernel.c

--- kernel.c
+++ kernel.c
@@ -8,18 +8,52 @@ const uint32_t multiboot_header[] = {
     -(0x1BADB002)
 };
 
+/* 行・列を管理する */
+#define VGA_WIDTH  80
+#define VGA_HEIGHT 25
+
+volatile uint16_t* vga = (uint16_t*)0xB8000;
+
+int cursor_row = 0;
+int cursor_col = 0;
+uint8_t current_color = 0x0F; // 白文字・黒背景
+
+/* 1文字出力関数を作る */
+void putchar(char c) {
+    if (c == '\n') {
+        cursor_row++;
+        cursor_col = 0;
+        return;
+    }
+
+    int index = cursor_row * VGA_WIDTH + cursor_col;
+    vga[index] = (current_color << 8) | c;
+
+    cursor_col++;
+
+    if (cursor_col >= VGA_WIDTH) {
+        cursor_col = 0;
+        cursor_row++;
+    }
+}
+
+/* 文字列出力関数 */
+void print(const char* str) {
+    for (int i = 0; str[i] != '\0'; i++) {
+        putchar(str[i]);
+    }
+}
+
 void kernel_main(void) {
-    volatile uint16_t* vga = (uint16_t*)0xB8000;
 
     // 画面クリア
-    for (int i = 0; i < 80*25; i++) {
-        vga[i] = (0x0F << 8) | ' ';
+    for (int i = 0; i < VGA_WIDTH * VGA_HEIGHT; i++) {
+        vga[i] = (current_color << 8) | ' ';
     }
 
-    const char* msg = "Hello, My OS";
-    for (int i = 0; msg[i] != '\0'; i++) {
-        vga[i] = (0x0F << 8) | msg[i];
-    }
+    print("Hello, My OS\n");
+    print("Second Line\n");
+    print("Third Line");
 
     while (1) {
         __asm__("hlt");
