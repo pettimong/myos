908eb9b84cfa1f60623677af15b3f954bfec4706
pettimong-arch <260029462+pettimong@users.noreply.github.com>
2026-03-22 23:35:34 +0900

    minor fixes

--- boot.asm
+++ boot.asm
@@ -1,36 +1,11 @@
-org 0x7C00
-bits 16
+[BITS 16]
+[ORG 0x7C00]
 
-start:
-    mov [BOOT_DRIVE], dl
+	mov ah, 0x0E
+	mov al, 'P'
+	int 0x10
 
-    cli
-    xor ax, ax
-    mov ds, ax
-    mov es, ax
-
-    call load_kernel
-
-    ; ★ ここが最重要
-    jmp 0x0000:0x8000
-
-hang:
-    hlt
-    jmp hang
-
-load_kernel:
-    mov ah, 0x02
-    mov al, 20          ; 読み込むセクタ数
-    mov ch, 0
-    mov cl, 2
-    mov dh, 0
-    mov dl, [BOOT_DRIVE]
-    mov bx, 0x8000
-    int 0x13
-	jc load_kernel
-    ret
-
-BOOT_DRIVE db 0
+	jmp $
 
 times 510-($-$$) db 0
 dw 0xAA55
